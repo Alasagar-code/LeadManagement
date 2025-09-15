f9b499d6a0ae9b874b7254b41ec44d1af0d8bb4a
Alasagar-code <alasagar0710@gmail.com>
2025-09-15 11:21:06 +0530

    Added BackendCall

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 // src/api/api.js
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000/api";
+const API_BASE = import.meta.env.VITE_API_BASE || "https://leadmanagement-a7y0.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_BASE,
